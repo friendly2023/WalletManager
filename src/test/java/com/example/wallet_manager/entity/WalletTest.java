9885520f9b2f83abfb7a54c2b289e0dd8ed05520
Vladimir <kazimirov.2013@mail.ru>
2025-05-07 12:51:10 +0300

    Add: добавлен тест для проверки корректрости операций с малыми суммами

---
+++ src/test/java/com/example/wallet_manager/entity/WalletTest.java
@@ -0,0 +1,32 @@
+package com.example.wallet_manager.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WalletTest {
+
+    @Test
+    void workingWithSmallAmounts() {
+        Wallet wallet1 = new Wallet();
+        wallet1.setBalance(BigDecimal.valueOf(0.01));
+
+        Wallet wallet2 = new Wallet();
+        wallet2.setBalance(BigDecimal.valueOf(0.02));
+
+        BigDecimal amount = wallet1.getBalance().add(wallet2.getBalance());
+        BigDecimal difference = wallet2.getBalance().subtract(wallet1.getBalance());
+        BigDecimal complexCalculation = BigDecimal.valueOf(9.99).add(BigDecimal.valueOf(7.77))
+                                                                .subtract(BigDecimal.valueOf(12.12));
+
+        System.out.println("0.03 : " + amount);
+        System.out.println("0.01 : " + difference);
+        System.out.println("5.64 : " + complexCalculation);
+
+        assertEquals(BigDecimal.valueOf(0.03), amount);
+        assertEquals(BigDecimal.valueOf(0.01), difference);
+        assertEquals(BigDecimal.valueOf(5.64), complexCalculation);
+    }
+}
